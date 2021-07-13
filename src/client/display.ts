0536ad804400545e65fb1d44216c1ddb8a5d9e4f
Anupam Ashish Minz <anupamminz300am@gmail.com>
2021-07-13 11:41:54 +0530

    display refator

--- src/client/display.ts
+++ src/client/display.ts
@@ -24,10 +24,14 @@ export default class Display {
         this.ctx.clearRect(0, 0, this.cols, this.rows);
         this.ctx.fillStyle = "#aaa";
     }
+
     setSprite(x: number, y: number, spriteData: Uint8Array): number {
         const width = 8;
         const height = spriteData.length;
 
+        // 0x80 = 10000000
+        // take right most bit of the rows and shift all bits left 
+        // set pixel if bit is 1
         for (let i=0; i<height; i++) {
             let spriteRow = spriteData[i];
             for (let j=0; j<width; j++) {
@@ -39,7 +43,9 @@ export default class Display {
         }
         return 0;
     }
+
     setPixel (x: number, y: number): number {
+        // rollover 
         x = Math.abs(x) % this.cols;
         y = Math.abs(y) % this.rows;
 
@@ -49,12 +55,15 @@ export default class Display {
 
         return this.displayBuffer[pixelLoc] === 0 ? 1 : 0;
     }
+
     clearBuffer () {
         this.displayBuffer = this.displayBuffer.fill(0);
     }
+
     clearScreen () {
         this.ctx.clearRect(0, 0, this.cols * this.scale, this.rows * this.scale);
     }
+
     draw () {
         this.clearScreen();
         this.drawBoard();
@@ -66,11 +75,13 @@ export default class Display {
             }
         }
     }
+
     testRender () {
         this.setPixel(0, 0);
         this.setPixel(5, 2);
         this.draw();
     }
+
     testSprite () {
         const sprite0 = Uint8Array.from([ 0xF0, 0x90, 0x90, 0x90, 0xF0 ]);
         this.setSprite(0, 0, sprite0);
@@ -78,20 +89,22 @@ export default class Display {
         const sprite2 = Uint8Array.from([ 0xF0, 0x10, 0xF0, 0x80, 0xF0 ]);
         this.setSprite(70, 0, sprite2);
     }
-    drawBoard() {
-      const bw = this.canvas.width;
-      const bh = this.canvas.height;
-      const p = 0;
-      for (var x = 0; x <= bw; x += this.scale) {
-          this.ctx.moveTo(0.5 + x + p, p);
-          this.ctx.lineTo(0.5 + x + p, bh + p);
-      }
 
-      for (var x = 0; x <= bh; x += this.scale) {
-          this.ctx.moveTo(p, 0.5 + x + p);
-          this.ctx.lineTo(bw + p, 0.5 + x + p);
-      }
-      this.ctx.strokeStyle = "black";
-      this.ctx.stroke();
+    drawBoard() {
+        // I don't know what 0.5 is doing here
+        const bw = this.canvas.width;
+        const bh = this.canvas.height;
+        const p = 0;
+        for (let x = 0; x <= bw; x += this.scale) {
+            this.ctx.moveTo(0.5 + x + p, p);
+            this.ctx.lineTo(0.5 + x + p, bh + p);
+        }
+
+        for (let x = 0; x <= bh; x += this.scale) {
+            this.ctx.moveTo(p, 0.5 + x + p);
+            this.ctx.lineTo(bw + p, 0.5 + x + p);
+        }
+        this.ctx.strokeStyle = "black";
+        this.ctx.stroke();
     }
 }
